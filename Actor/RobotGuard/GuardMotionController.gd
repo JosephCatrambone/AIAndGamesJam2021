a077d48b28fb24a505ed6a2db2fb1698fbd47f7c
Joseph Catrambone <jo.jcat@gmail.com>
2021-05-30 23:34:07 -0700

    Add robot guard and camera smoothing.  Add a background tileset, too, but that's not committed.

---
+++ Actor/RobotGuard/GuardMotionController.gd
@@ -0,0 +1,121 @@
+extends MotionController
+
+# Handles most of the guard AI.  Simple state machine.
+# Eventually expose this to a CPU.
+
+onready var vision_cone_display:Polygon2D = $VisionConeDisplay
+onready var vision_cone:Area2D = $VisionCone
+onready var ray:RayCast2D = $RayCast2D
+export(float) var look_distance:float = 10
+var potential_visible_bodies = []
+
+enum GuardState { 
+	IDLE,
+	WANDER,
+	FOLLOW_PATH,
+	PURSUE,
+}
+var state = GuardState.WANDER
+
+# For all states, we need to track DX/DY.
+# A bit of duplicative code because the parent actor has 'facing' and 'movement', but whatever.
+var dxdy:Vector2 = Vector2()
+
+# For pathing.
+export(float) var distance_before_popping_nav_point:float = 1.0
+var active_path:PoolVector2Array
+
+# For wandering.
+export(float) var time_between_turns:float = 1.0
+var time_to_next_turn:float = 0.0
+
+func _ready():
+	vision_cone.connect("body_entered", self, "add_visible_candidate")
+	vision_cone.connect("body_exited", self, "remove_visible_candidate")
+
+func _process(delta):
+	match self.state:
+		GuardState.IDLE:
+			pass
+		GuardState.WANDER:
+			_process_wander(delta)
+		GuardState.FOLLOW_PATH:
+			_process_follow(delta)
+		GuardState.PURSUE:
+			pass
+
+func _process_wander(delta):
+	time_to_next_turn -= delta
+	if time_to_next_turn < 0:
+		time_to_next_turn = time_between_turns
+		var rng = RandomNumberGenerator.new()
+		var facing:Vector2 = get_parent().face_direction
+		var angle = facing.angle()
+		angle += PI/2.0 * rng.randi_range(0, 3)
+		dxdy = Vector2(cos(angle), sin(angle))
+		if dxdy.x == 0 and dxdy.y == -1:
+			print("Facing UP")
+		elif dxdy.x == 0 and dxdy.y == 1:
+			print("Facing DOWN")
+		elif dxdy.x == -1 and dxdy.y == 0:
+			print("Facing LEFT")
+		elif dxdy.x == 1 and dxdy.y == 0:
+			print("Facing RIGHT")
+		else:
+			print("Facing " + str(angle) + " | " + str(dxdy))
+
+func _process_follow(delta):
+	# If the active point is none AND the active path is empty, we're done.
+	if self.active_path.empty():
+		self.state = GuardState.WANDER
+		return
+	# Otherwise, move towards an active path.
+	var delta_position = self.active_path[0]
+	if delta_position < distance_before_popping_nav_point*delta:
+		self.active_path.remove(0)
+		return  # Resume next cycle.
+	self.dxdy = Vector2(sign(delta_position.x), sign(delta_position.y))
+
+func start_follow(target:Vector2):
+	# TODO: Don't hardcode this name.
+	var navmesh:Navigation2D = get_tree().get_root().get_node("NavMesh")
+	self.active_path = navmesh.get_simple_path(self.global_position, target)
+
+func add_visible_candidate(candidate):
+	self.potential_visible_bodies.append(candidate)
+
+func remove_visible_candidate(candidate):
+	if candidate in self.potential_visible_bodies:
+		self.potential_visible_bodies.erase(candidate)
+	
+func update():
+	# Get facing direction from parent.  Let the parent deal with rotation.
+	var facing:Vector2 = get_parent().face_direction
+	
+	# Set the rotation of the vision cone and the polygon 2d to the rotation of the parent.
+	var angle = facing.angle()
+	vision_cone.rotation = angle
+	vision_cone_display.rotation = angle
+	
+	# Update the raycast forward.
+	#ray.cast_to = facing.normalized() * look_distance
+	# Go over the list of things we might see and raycast to them.
+	for body in self.potential_visible_bodies:
+		ray.cast_to = body.global_position  # This may be the wrong coordinate system.
+		ray.force_raycast_update()
+		if ray.get_collider() != null:
+			pass
+			#cpu.memory[BUMP_REPORT] = 1
+			# CPU code will clear bump.
+
+func get_x_axis() -> float:
+	return dxdy.x
+
+func get_y_axis() -> float:
+	return dxdy.y
+
+func get_fire_button() -> bool:
+	return false
+
+func get_interact_button() -> bool:
+	return false
---
+++ Camera/CameraController.gd
@@ -0,0 +1,36 @@
+extends Camera2D
+
+export(float) var max_smooth_follow:float = 128 # If a target is more than this many units away, just snap to them.
+export(float) var lookahead_distance:float = 8
+export(NodePath) var follow_target_path:NodePath
+var follow_target:Node2D
+
+var previous_position:Vector2
+
+func _ready():
+	follow_target = get_node(follow_target_path)
+
+func set_position(target:Vector2):
+	self.global_position = target
+	self.previous_position = target
+
+func _process(delta):
+	# Figure out which way we're going and move in that direction.
+	#var new_position = follow_target.global_position
+	#var delta_1 = 
+	var delta_position:Vector2 = follow_target.global_position - self.global_position
+	if delta_position.length_squared() > max_smooth_follow:
+		self.smoothing_enabled = false
+		self.global_position = follow_target.global_position
+		self.smoothing_enabled = true
+	else:
+		self.global_position = follow_target.global_position
+		
+	# Offset will let us look in the direction we're going.
+	var velocity = delta_position / max(0.1, delta)
+	var previous_velocity = (self.global_position - self.previous_position) / max(0.1, delta)
+	var lookahead = lookahead_distance * (previous_velocity + velocity) / 2
+	#self.offset = 0.9*self.offset + 0.1*lookahead
+	self.offset = lookahead*0.02 + self.offset*0.98
+	
+	self.previous_position = self.global_position
